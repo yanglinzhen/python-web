dc37088f3a668e633bcc0bff2e0b3e7dc18002bc
yanglinzhen <yanglinzhen@outlook.com>
2019-04-22 17:40:57 +0800

    addded deployment configs

--- python-web/app/__init__.py
+++ python-web/app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+import os
 
 db = SQLAlchemy()
 
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
+
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder=static_file_dir)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
--- python-web/app/main/views.py
+++ python-web/app/main/views.py
@@ -1,9 +1,12 @@
 from . import main
-
-@main.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
+import app
+from flask import send_from_directory
 
 @main.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}</h1>'.format(name)
+
+@main.route('/', defaults={'path': ''})
+@main.route('/<path:path>')
+def catch_all(path):
+    return send_from_directory(app.static_file_dir, 'index.html')
